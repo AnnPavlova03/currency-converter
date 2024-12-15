9e7bf0750bfba89760dd758b7bafd6b2ec49c392
Anna <anneta_03@mail.ru>
2024-12-15 19:17:11 +0300

    feat: add header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import styles from './header.module.scss'
+import logo from '../../assets/svg/logo.svg'
 
 export const Header = () => {
 	return (
 		<header className={styles.header}>
-			<img />
+			<a href='#'>
+				<img src={logo} alt='логотип конвертации' />
+			</a>
 			<div>
 				<button className={`${styles.button}`}> Вход</button>
 				<button className={`${styles.button} ${styles.button_registration}`}>
--- src/pages/main/home.tsx
+++ src/pages/main/home.tsx
@@ -1,11 +1,5 @@
 import React from 'react'
 
 export const Home = () => {
-	return (
-		<div>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, cum vitae
-			assumenda id consequatur ad aliquid illo repellendus similique modi iure
-			alias, quas officiis nesciunt ratione porro cupiditate doloribus sequi!
-		</div>
-	)
+	return <></>
 }
