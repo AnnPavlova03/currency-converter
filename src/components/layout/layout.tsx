db2533cc51839dc25fde4ebea464b16d12f7f7f2
sashakostiukova <sashakostiukova@yandex.ru>
2024-12-24 21:21:07 +0300

    refactor(login and register, globals, variables): refactor login and register, edit global styles
    refactor login and register, add validation; edit global styles, add error-color variable

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,6 +2,11 @@ import React, { FC, memo, ReactNode } from 'react'
 import { Header } from '../header'
 import styles from './layout.module.scss'
 
+/////////////
+import { Login } from '../../pages/login'
+import { Register } from '../../pages/register'
+///////////
+
 type TLayout = {
 	children: ReactNode
 }
@@ -9,6 +14,8 @@ export const Layout: FC<TLayout> = memo(({ children }) => {
 	return (
 		<>
 			<Header />
+			<Login />
+			<Register />
 			<main className={styles.main}>{children}</main>
 		</>
 	)
